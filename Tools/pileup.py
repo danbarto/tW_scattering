ca2d4b26a86186d4d15100ee274159f337393cf8
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2022-05-17 14:03:37 -0700

    OTF PU reweighting -> correctionlib

--- Tools/pileup.py
+++ Tools/pileup.py
@@ -2,25 +2,30 @@ import os
 import awkward as ak
 import numpy as np
 
-from yahist import Hist1D
-# FIXME there should be a correctionlib version of this now
+import correctionlib
 
 class pileup:
     
-    def __init__(self, year, UL=True):
+    def __init__(self, year, UL=True, era=None):
         self.weight = {}
+        self.UL = UL
 
         if UL:
             if year == 2016:
-                pass
+                if era=='APV':
+                    SF_file = os.path.expandvars('$TWHOME/jsonpog-integration/POG/LUM/2016preVFP_UL/')
+                    self.reader = correctionlib.CorrectionSet.from_file(os.path.join(SF_file, "puWeights.json.gz"))['Collisions16_UltraLegacy_goldenJSON']
+
+                else:
+                    SF_file = os.path.expandvars('$TWHOME/jsonpog-integration/POG/LUM/2016postVFP_UL/')
+                    self.reader = correctionlib.CorrectionSet.from_file(os.path.join(SF_file, "puWeights.json.gz"))['Collisions16_UltraLegacy_goldenJSON']
             elif year == 2017:
-                pass
+                SF_file = os.path.expandvars('$TWHOME/jsonpog-integration/POG/LUM/2017_UL/')
+                self.reader = correctionlib.CorrectionSet.from_file(os.path.join(SF_file, "puWeights.json.gz"))['Collisions17_UltraLegacy_goldenJSON']
             elif year == 2018:
-                mc          = Hist1D.from_json(os.path.expandvars('$TWHOME/data/PU/Summer20UL18_mc.json'))
-                data        = Hist1D.from_json(os.path.expandvars('$TWHOME/data/PU/Summer20UL18_data.json'))
-                data_up     = Hist1D.from_json(os.path.expandvars('$TWHOME/data/PU/Summer20UL18_data_up.json'))
-                data_down   = Hist1D.from_json(os.path.expandvars('$TWHOME/data/PU/Summer20UL18_data_down.json'))
-                
+                SF_file = os.path.expandvars('$TWHOME/jsonpog-integration/POG/LUM/2018_UL/')
+                self.reader = correctionlib.CorrectionSet.from_file(os.path.join(SF_file, "puWeights.json.gz"))['Collisions18_UltraLegacy_goldenJSON']
+
         else:
             if year == 2016:
                 mc          = Hist1D.from_json(os.path.expandvars('$TWHOME/data/PU/Summer16_mc.json'))
@@ -38,12 +43,16 @@ class pileup:
                 data_up     = Hist1D.from_json(os.path.expandvars('$TWHOME/data/PU/Autumn18_data_up.json'))
                 data_down   = Hist1D.from_json(os.path.expandvars('$TWHOME/data/PU/Autumn18_data_down.json'))
 
-        self.weight['central']  = Hist1D.from_bincounts(np.nan_to_num(data.counts/mc.counts,nan=1,posinf=1, neginf=1), data.edges)
-        self.weight['up']       = Hist1D.from_bincounts(np.nan_to_num(data_up.counts/mc.counts,nan=1,posinf=1, neginf=1), data.edges)
-        self.weight['down']     = Hist1D.from_bincounts(np.nan_to_num(data_down.counts/mc.counts,nan=1,posinf=1, neginf=1), data.edges)
+            self.weight['central']  = Hist1D.from_bincounts(np.nan_to_num(data.counts/mc.counts,nan=1,posinf=1, neginf=1), data.edges)
+            self.weight['up']       = Hist1D.from_bincounts(np.nan_to_num(data_up.counts/mc.counts,nan=1,posinf=1, neginf=1), data.edges)
+            self.weight['down']     = Hist1D.from_bincounts(np.nan_to_num(data_down.counts/mc.counts,nan=1,posinf=1, neginf=1), data.edges)
 
     def reweight(self, nvtx, to='central'):
-        return self.weight[to].lookup(ak.to_numpy(nvtx))
+        if self.UL:
+            if to == 'central': to = 'nominal'
+            return self.reader.evaluate(nvtx, to)
+        else:
+            return self.weight[to].lookup(ak.to_numpy(nvtx))
 
 
 if __name__ == '__main__':
@@ -52,21 +61,20 @@ if __name__ == '__main__':
 
     pu = pileup(2018, UL=True)
 
-
     ## Load a single file here, get leptons, eval SFs just to be sure everything works
     from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 
-    from Tools.samples import get_babies
+    from Tools.helpers import get_samples
     from Tools.objects import Collections
     
     import awkward as ak
-    
-    fileset_all = get_babies('/hadoop/cms/store/user/dspitzba/nanoAOD/ttw_samples/topW_v0.3.3_dilep/', year='UL2018')
-    
+    samples = get_samples("samples_UL18.yaml")
+    f_in    = samples['/ceph/cms/store/user/dspitzba/ProjectMetis/TTWToLNu_TtoAll_aTtoLep_5f_EFT_NLO_RunIISummer20UL18_NanoAODv9_NANO_v14/']['files'][0]
+
     # load a subset of events
     n_max = 5000
     events = NanoEventsFactory.from_root(
-        fileset_all['TTW'][0],
+        f_in,
         schemaclass = NanoAODSchema,
         entry_stop = n_max).events()
 
@@ -75,7 +83,7 @@ if __name__ == '__main__':
     print (pu_weight)
 
     print ("Example weights for some events.")
-    pu_weight = pu.reweight(events.Pileup.nTrueInt)
+    pu_weight = pu.reweight(events.Pileup.nTrueInt.to_numpy())
     print (pu_weight)
 
 
