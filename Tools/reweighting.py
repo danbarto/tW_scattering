6a0b739b5c6a68ac434d2c8d3c3c569ed46f81f2
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2022-07-14 09:20:21 -0700

    reference point for just 2D

--- Tools/reweighting.py
+++ Tools/reweighting.py
@@ -5,15 +5,18 @@ def get_coordinates(points):
     vals = [ float(x.replace('p','.')) for x in points.split('_')[1::2] ]
     return tuple(vals)
 
-def get_coordinates_and_ref(f_in):
+def get_coordinates_and_ref(f_in, 2D=False):
     import uproot
 
     tree = uproot.open(f_in)['Events']
     weights = [ x for x in tree.keys() if x.startswith('LHEWeight_c') ]
     #ref_point = [ x for x in tree.keys() if x.startswith('ref_point') ][0]
     #ref_point = ref_point.replace('ref_point_','')
-    ref_point = "ctZ_2p_cpt_4p_cpQM_4p_cpQ3_4p_ctW_2p_ctp_2p"
-    
+    if 2D:
+        ref_point = "cpt_0p_cpQM_0p"
+    else:
+        ref_point = "ctZ_2p_cpt_4p_cpQM_4p_cpQ3_4p_ctW_2p_ctp_2p"
+
     coordinates = [get_coordinates(weight) for weight in weights]
     ref_coordinates = [ float(x.replace('p','.')) for x in ref_point.split('_')[1::2] ]
     
