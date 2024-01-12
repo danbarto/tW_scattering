0b67bd1f495d8d2ba4d244907f18405c1b49c611
danbarto <daniel.spitzbart@cern.ch>
2024-01-12 14:17:06 -0600

    update trigger SF class

--- analysis/Tools/trigger_scalefactors.py
+++ analysis/Tools/trigger_scalefactors.py
@@ -1,66 +1,91 @@
 import os
 import awkward as ak
-from coffea.lookup_tools import extractor
+import gzip
+import pickle
+import numpy as np
+from coffea import hist as chist
+from coffea.lookup_tools import extractor, dense_lookup
 
 here = os.path.dirname(os.path.abspath(__file__))
+
+#### Functions needed, taken from TopEFT
+StackOverUnderflow = lambda v : [sum(v[0:2])] + v[2:-2] + [sum(v[-2:])]
+
+def GetClopperPearsonInterval(hnum, hden):
+    ''' Compute Clopper-Pearson interval from numerator and denominator histograms '''
+    num = list(hnum.values(overflow='all')[()])
+    den = list(hden.values(overflow='all')[()])
+    if isinstance(num, list) and isinstance(num[0], np.ndarray):
+        for i in range(len(num)):
+            num[i] = np.array(StackOverUnderflow(list(num[i])), dtype=float)
+            den[i] = np.array(StackOverUnderflow(list(den[i])), dtype=float)
+        den = StackOverUnderflow(den)
+        num = StackOverUnderflow(num)
+    else:
+        num = np.array(StackOverUnderflow(num), dtype=float)
+        den = np.array(StackOverUnderflow(den), dtype=float)
+    num = np.array(num)
+    den = np.array(den)
+    num[num>den] = den[num > den]
+    down, up = chist.clopper_pearson_interval(num, den)
+    ratio = np.array(num, dtype=float) / den
+    return [ratio, down, up]
+
+def GetEff(num, den):
+    ''' Compute efficiency values from numerator and denominator histograms '''
+    ratio, down, up = GetClopperPearsonInterval(num, den)
+    axis = num.axes()[0].name
+    bins = num.axis(axis).edges()
+    x    = num.axis(axis).centers()
+    xlo  = bins[:-1]
+    xhi  = bins[1:]
+    return [[x, xlo-x, xhi-x],[ratio, down-ratio, up-ratio]]
+
+def GetSFfromCountsHisto(hnumMC, hdenMC, hnumData, hdenData):
+    ''' Compute scale factors from efficiency histograms for data and MC '''
+    Xmc, Ymc = GetEff(hnumMC, hdenMC)
+    Xda, Yda = GetEff(hnumData, hdenData)
+    ratio, do, up = GetRatioAssymetricUncertainties(Yda[0], Yda[1], Yda[2], Ymc[0], Ymc[1], Ymc[2])
+    return ratio, do, up
+
+def GetRatioAssymetricUncertainties(num, numDo, numUp, den, denDo, denUp):
+    ''' Compute efficiencies from numerator and denominator counts histograms and uncertainties '''
+    ratio = num / den
+    uncUp = ratio * np.sqrt(numUp * numUp + denUp * denUp)
+    uncDo = ratio * np.sqrt(numDo * numDo + denDo * denDo)
+    return ratio, -uncDo, uncUp
+
 
 class triggerSF:
 
     def __init__(self, year=2016):
         self.year = year
-          
-        triggerSF_2016 = os.path.join(here, "data/trigger/TriggerSF_2016.root")
-        triggerSF_2017 = os.path.join(here, "data/trigger/TriggerSF_2017.root")
-        triggerSF_2018 = os.path.join(here, "data/trigger/TriggerSF_2018.root")
-        
-        
-        self.ext = extractor()
-        # several histograms can be imported at once using wildcards (*)
-        if self.year == 2016:
-            self.ext.add_weight_sets([
-                "mumu_2016 h2D_SF_mumu_lepABpt_FullError %s"%triggerSF_2016,
-                "mumu_2016_error h2D_SF_mumu_lepABpt_FullError_error %s"%triggerSF_2016,
 
-                "emu_2016 h2D_SF_emu_lepABpt_FullError %s"%triggerSF_2016,
-                "emu_2016_error h2D_SF_emu_lepABpt_FullError_error %s"%triggerSF_2016,
+        inputfiles = {
+            2016: os.path.join(here, "data/trigger/triggerSF_2016.pkl.gz"),
+            2017: os.path.join(here, "data/trigger/triggerSF_2017.pkl.gz"),
+            2018: os.path.join(here, "data/trigger/triggerSF_2018.pkl.gz"),
+        }
 
-                "ee_2016 h2D_SF_ee_lepABpt_FullError %s"%triggerSF_2016,
-                "ee_2016_error h2D_SF_ee_lepABpt_FullError_error %s"%triggerSF_2016,
+        with gzip.open(inputfiles[self.year]) as fin:
+            hin = pickle.load(fin)
 
-            ])
-            
+        axisY = 'l1pt'
 
-        elif self.year == 2017:
-            self.ext.add_weight_sets([
-                "mumu_2017 h2D_SF_mumu_lepABpt_FullError %s"%triggerSF_2017,
-                "mumu_2017_error h2D_SF_mumu_lepABpt_FullError_error %s"%triggerSF_2017,
-
-                "emu_2017 h2D_SF_emu_lepABpt_FullError %s"%triggerSF_2017,
-                "emu_2017_error h2D_SF_emu_lepABpt_FullError_error %s"%triggerSF_2017,
-
-                "ee_2017 h2D_SF_ee_lepABpt_FullError %s"%triggerSF_2017,
-                "ee_2017_error h2D_SF_ee_lepABpt_FullError_error %s"%triggerSF_2017,
-
-            ])
-        elif self.year == 2018:
-            self.ext.add_weight_sets([
-                "mumu_2018 h2D_SF_mumu_lepABpt_FullError %s"%triggerSF_2018,
-                "mumu_2018_error h2D_SF_mumu_lepABpt_FullError_error %s"%triggerSF_2018,
-
-                "emu_2018 h2D_SF_emu_lepABpt_FullError %s"%triggerSF_2018,
-                "emu_2018_error h2D_SF_emu_lepABpt_FullError_error %s"%triggerSF_2018,
-
-                "ee_2018 h2D_SF_ee_lepABpt_FullError %s"%triggerSF_2018,
-                "ee_2018_error h2D_SF_ee_lepABpt_FullError_error %s"%triggerSF_2018,
-
-            ])
+        self.lookups = {}
+        for ch in ['mm', 'em', 'ee']:
+            self.lookups[ch] = {}
+            h = hin['2l'][ch]
+            ratio, do, up = GetSFfromCountsHisto(h['hmn'], h['hmd'], h['hdn'], h['hdd'])
+            ratio = np.nan_to_num(ratio, 1.)
+            do = np.nan_to_num(do, 0.)
+            up = np.nan_to_num(up, 0.)
+            self.lookups[ch]['central'] = dense_lookup.dense_lookup(ratio, [h['hmn'].axis('l0pt').edges(), h['hmn'].axis(axisY).edges()])
+            self.lookups[ch]['up'] = dense_lookup.dense_lookup(up, [h['hmn'].axis('l0pt').edges(), h['hmn'].axis(axisY).edges()])
+            self.lookups[ch]['down'] = dense_lookup.dense_lookup(do, [h['hmn'].axis('l0pt').edges(), h['hmn'].axis(axisY).edges()])
 
 
-        self.ext.finalize()
-
-        self.evaluator = self.ext.make_evaluator()
-
-    def get(self, ele, mu, variation='central'):
+    def get_dilep(self, ele, mu, variation='central'):
         multiplier = {'central': 0, 'up': 1, 'down': -1}
 
         from analysis.Tools.helpers import pad_and_flatten
@@ -72,40 +97,16 @@ class triggerSF:
         lep = lep[ak.argsort(lep.pt, ascending=False)]
         l0_is_ele = (abs(pad_and_flatten(lep[:,0:1].pdgId))==11)
         l1_is_ele = (abs(pad_and_flatten(lep[:,1:2].pdgId))==11)
-                
-        if self.year == 2016:
-            
-            ee_sf = self.evaluator["ee_2016"](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
-            ee_sf = ee_sf + multiplier[variation]*self.evaluator["ee_2016_error"](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
 
-            emu_sf = self.evaluator["emu_2016"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
-            emu_sf = emu_sf + multiplier[variation]*self.evaluator["emu_2016_error"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
+        ee_sf = self.lookups['ee']['central'](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
+        ee_sf = 1 + multiplier[variation]*self.lookups['ee'][multiplier](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
 
-            mumu_sf = self.evaluator["mumu_2016"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(mu[:,1:2].pt))            
-            mumu_sf = mumu_sf + multiplier[variation]*self.evaluator["mumu_2016_error"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(mu[:,1:2].pt))
-            
-        elif self.year == 2017:
-                                          
-            ee_sf = self.evaluator["ee_2017"](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
-            ee_sf = ee_sf + multiplier[variation]*self.evaluator["ee_2017_error"](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
+        em_sf = self.lookups['em']['central'](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
+        em_sf = 1 + multiplier[variation]*self.lookups['em'][multiplier](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
 
-            emu_sf = self.evaluator["emu_2017"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
-            emu_sf = emu_sf + multiplier[variation]*self.evaluator["emu_2017_error"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
+        mm_sf = self.lookups['mm']['central'](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
+        mm_sf = 1 + multiplier[variation]*self.lookups['mm'][multiplier](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
 
-            mumu_sf = self.evaluator["mumu_2017"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(mu[:,1:2].pt))            
-            mumu_sf = mumu_sf + multiplier[variation]*self.evaluator["mumu_2017_error"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(mu[:,1:2].pt))
-            
-        elif self.year == 2018:
-            
-            ee_sf = self.evaluator["ee_2018"](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
-            ee_sf = ee_sf + multiplier[variation]*self.evaluator["ee_2018_error"](pad_and_flatten(ele[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
-
-            emu_sf = self.evaluator["emu_2018"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
-            emu_sf = emu_sf + multiplier[variation]*self.evaluator["emu_2018_error"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(ele[:,1:2].pt))
-
-            mumu_sf = self.evaluator["mumu_2018"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(mu[:,1:2].pt))            
-            mumu_sf = mumu_sf + multiplier[variation]*self.evaluator["mumu_2018_error"](pad_and_flatten(mu[:,0:1].pt), pad_and_flatten(mu[:,1:2].pt))
-            
 
         sf = (ee_sf*(l0_is_ele&l1_is_ele)) + (emu_sf*(l0_is_ele^l1_is_ele)) + (mumu_sf*(~l0_is_ele&~l1_is_ele))
         #ee_mm_em = ak.concatenate([choose(ele), choose(mu), cross(mu, ele)], axis=1)
@@ -126,25 +127,25 @@ if __name__ == '__main__':
     sf17 = triggerSF(year=2017)
     sf18 = triggerSF(year=2018)
 
-    print("Evaluators found for 2016:")
-    for key in sf16.evaluator.keys():
-        print("%s:"%key, sf16.evaluator[key])
+    #print("Evaluators found for 2016:")
+    #for key in sf16.evaluator.keys():
+    #    print("%s:"%key, sf16.evaluator[key])
 
-    print("Evaluators found for 2017:")
-    for key in sf17.evaluator.keys():
-        print("%s:"%key, sf17.evaluator[key])
+    #print("Evaluators found for 2017:")
+    #for key in sf17.evaluator.keys():
+    #    print("%s:"%key, sf17.evaluator[key])
 
-    print("Evaluators found for 2018:")
-    for key in sf18.evaluator.keys():
-        print("%s:"%key, sf18.evaluator[key])
+    #print("Evaluators found for 2018:")
+    #for key in sf18.evaluator.keys():
+    #    print("%s:"%key, sf18.evaluator[key])
         
     ## Load a single file here, get leptons, eval SFs just to be sure everything works
     from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 
-    from Tools.objects import Collections
-    from Tools.config_helpers import loadConfig, make_small, load_yaml, data_path
-    from Tools.helpers import get_samples
-    from Tools.nano_mapping import make_fileset
+    from analysis.Tools.objects import Collections
+    from analysis.Tools.config_helpers import loadConfig, make_small, load_yaml, data_path
+    from analysis.Tools.helpers import get_samples
+    from analysis.Tools.nano_mapping import make_fileset
     
     import awkward as ak
 
